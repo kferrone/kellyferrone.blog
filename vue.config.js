2b100437c7b83124b1dd402b7c0020f342886c62
Kelly Ferrone <4399427+kferrone@users.noreply.github.com>
2019-03-09 15:12:12 -0700

    coffee and stylus added

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('coffee')
+      .test(/\.coffee$/)
+      .use('coffee-loader')
+      .loader('coffee-loader')
+      .end()
+  },
   css: {
     loaderOptions: {
       // pass options to sass-loader
