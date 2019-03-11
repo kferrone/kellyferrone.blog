f46722bccca0936afa2f91396dc69d9b57baa037
Kelly Ferrone <4399427+kferrone@users.noreply.github.com>
2019-03-11 10:24:43 -0600

    forgot to push dynamo view

---
+++ src/views/Dynamo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="dynamo">
+    <h1>This is the dynamo page</h1>
+  </div>
+</template>
+
+<script lang="coffee">
+export default
+    name: 'dynamo'
+    beforeRouteUpdate: (to, from, next) =>
+        console.log(to,from,next)
+</script>
